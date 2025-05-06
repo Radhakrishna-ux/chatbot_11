1c546ada73d065041e69fa4c405bb347224b825a
Radhakrishna-ux <radhakrishnaven05@gmail.com>
2025-05-05 23:58:32 -0400

    about page

--- frontend/script.js
+++ frontend/script.js
@@ -42,11 +42,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         try {
             // Send the user's message to the Flask API endpoint
-            const response = await fetch(//"/api/chat", {
-             //"http://localhost:5000/api/chat"
-               "https://chatbot-11-2.onrender.com/api/chat" , {
-             //  { //--- for local (change port as per your device)
-                // https://smart-assistantbot.onrender.com/api/chat" --- for online
+            const response = await fetch(//"api/chat", 
+            //  "http://127.0.0.1:5000" , {
+                "https://chatbot-11-2.onrender.com/api/chat" , {
+            
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ message: userText })
